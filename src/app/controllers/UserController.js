1401cb552aac600478103344fb8bbab8cc0b2901
Luan <luan10017@hotmail.com>
2020-12-22 21:11:48 -0300

    Post para users

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,5 +1,12 @@
+const User = require("../model/User")
+
 module.exports = {
     registerForm(req, res) {
         return res.render("user/register.njk")
+    }, 
+    async post(req, res) {
+        
+
+        return res.send('passed')
     }
 }
