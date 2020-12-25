2052e30c41149cc84063f08a4c25efc6a5575b11
Luan <luan10017@hotmail.com>
2020-12-25 07:32:24 -0300

    Adicionei table das sessions

--- database.sql
+++ database.sql
@@ -72,4 +72,12 @@ FOR EACH ROW
 EXECUTE PROCEDURE trigger_set_timestamp();
 
 
+--connect pg simple table
+CREATE TABLE "session" (
+  "sid" varchar NOT NULL COLLATE "default",
+	"sess" json NOT NULL,
+	"expire" timestamp(6) NOT NULL
+)
+WITH (OIDS=FALSE);
 
+ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
