02beccf767feeca58569522cc3a2adcf66bbdcbd
Luan <luan10017@hotmail.com>
2020-12-25 09:46:37 -0300

    Habilitei rota put do UserController

--- src/routes/users.js
+++ src/routes/users.js
@@ -21,10 +21,9 @@ routes.post('/password-reset', SessionController.reset)
 // user register UserController
 routes.get('/register', UserController.registerForm)
 routes.post('/register', Validator.post, UserController.post)
-routes.get('/', UserController.show)
-
-/* routes.put('/', UserController.update)
-routes.delete('/', UserController.delete) */
+routes.get('/',Validator.show, UserController.show)
+routes.put('/', UserController.update)
+/* routes.delete('/', UserController.delete) */
 
 
 module.exports = routes
