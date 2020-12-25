4a3660b340662d1ae2815114410dcf2f15b39c24
Luan <luan10017@hotmail.com>
2020-12-25 09:45:44 -0300

    Adicionei metodo update

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -6,14 +6,8 @@ module.exports = {
         return res.render("user/register.njk")
     },
     async show(req, res) {
-
-        const { userId: id } = req.session
-
-        const user = await User.findOne({ where: {id} })
-
-        if (!user) return res.render('user/register', {
-            error: "Usuário não encontrado!"
-        })
+        const { user } = req
+        
 
         user.cpf_cnpj = formatCpfCnpj(user.cpf_cnpj)
         user.cep = formatCep(user.cep)
@@ -26,5 +20,8 @@ module.exports = {
         req.session.userId = userId
 
         return res.redirect('/users')
+    },
+    async update(req, res) {
+        
     }
 }
