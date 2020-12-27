6f982510e487e481b5f789cadfe0f1955bf578cc
Luan10017 <32424296+Luan10017@users.noreply.github.com>
2020-12-27 15:18:11 -0300

    Adicionei logout

--- src/routes/users.js
+++ src/routes/users.js
@@ -6,18 +6,18 @@ const UserController = require('../app/controllers/UserController')
 
 const Validator = require('../app/validators/user')
 
-/* 
+
 // login/logout
-routes.get('/login', SessionController.loginForm)
-routes.post('/login', SessionController.login)
+/* routes.get('/login', SessionController.loginForm)
+routes.post('/login', SessionController.login) */
 routes.post('/logout', SessionController.logout)
 
 // reset password / forgot
-routes.get('/forgot-password', SessionController.forgotForm)
+/* routes.get('/forgot-password', SessionController.forgotForm)
 routes.get('/password-reset', SessionController.resetForm)
 routes.post('/forgot-password', SessionController.forgot)
-routes.post('/password-reset', SessionController.reset)
- */
+routes.post('/password-reset', SessionController.reset) */
+
 // user register UserController
 routes.get('/register', UserController.registerForm)
 routes.post('/register', Validator.post, UserController.post)
