319e1d7420306ab4aae218545a8c6532564d0e0e
Luan <luan10017@hotmail.com>
2020-12-30 10:47:51 -0300

    Adicionei funcionalidade de troca de senha

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,6 +1,7 @@
-const crypto = require('crypto')
 const User = require('../model/User')
 const mailer = require('../../lib/mailer')
+const crypto = require('crypto')
+const { hash } = require('bcryptjs')
 
 module.exports = {
     loginForm(req, res) {
@@ -53,6 +54,39 @@ module.exports = {
         }catch(err) {
             console.error(err)
             return res.render("session/forgot-password", {
+                error: "Erro inesperado, tente novamente"
+            })
+        }
+    },
+    resetForm(req, res) {
+        return res.render("session/password-reset", { token: req.query.token })
+    },
+    async reset(req,res){
+        const user = req.user
+
+        const { password, token } = req.body
+
+        try{
+            // cria um novo hash de senha
+            const newPassword = await hash(password, 8)
+            // atualiza o usuário 
+            await User.update(user.id, {
+                password: newPassword,
+                reset_token: "",
+                reset_token_expires: "",
+            })
+            // avisa o usuário que ele tem uma nova senha 
+            return res.render("session/login", {
+                user: req.body,
+                success: "Senha atualizada! Faça o seu login"
+            })
+
+
+        }catch(err) {
+            console.error(err)
+            return res.render("session/password-reset", {
+                user: req.body,
+                token,
                 error: "Erro inesperado, tente novamente"
             })
         }
