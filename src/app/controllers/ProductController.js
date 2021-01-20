e721cd726596b63c927fd20d435f596dbca0630b
Luan10017 <32424296+Luan10017@users.noreply.github.com>
2021-01-20 13:23:32 -0300

    Atualizando chamas dos models

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,23 +1,22 @@
-const { formatPrice, date } = require('../../lib/utils')
-
 const Category = require('../model/Category')
 const Procuct = require('../model/Product')
 const File = require('../model/File')
 const Product = require('../model/Product')
 
-module.exports = {
-    create(req, res) {
-        Category.all()
-        .then(function(results) {
+const { formatPrice, date } = require('../../lib/utils')
 
-            const categories = results.rows;
+module.exports = {
+    async create(req, res) {
+        try {
+            const categories = await Category.findAll()
             return res.render("products/create.njk", { categories })
-        }).catch(function(err) {
-            throw new Error(err)
-        })
+        } catch (error) {
+            console.error(error);
+        }
     },
     async post(req, res) {
-        const keys = Object.keys(req.body)
+        try {
+            const keys = Object.keys(req.body)
 
         for(key of keys) {
             if (req.body[key] == "") {
@@ -25,101 +24,132 @@ module.exports = {
             }
         }
 
-        req.body.user_id = req.session.userId
+        let { category_id, name, description, old_price, 
+        price, quantity, status } = req.body
 
-        let results = await Procuct.create(req.body)
-        const productId = results.rows[0].id
+        price = price.replace(/\D/g,"")
 
-        const filesPromise = req.files.map(file => File.create({...file, product_id: productId}))
+        const product_id = await Procuct.create({
+            category_id, 
+            user_id: req.session.userId,
+            name, 
+            description, 
+            old_price: old_price || price, 
+            price, 
+            quantity, 
+            status: status || 1
+        })
+
+        const filesPromise = req.files.map(file => 
+            File.create({ ...file, product_id }))
         await Promise.all(filesPromise)
 
-        return res.redirect(`/products/${productId}/edit`)
+        return res.redirect(`/products/${product_id}/edit`)
+        } catch (error) {
+            console.error(error);
+        }
     },
     async show(req,res) {
+        try {
+            const product = await Procuct.find(req.params.id)
 
-        let results = await Procuct.find(req.params.id)
-        const product = results.rows[0]
+            if(!product) return res.send('Product Not Found!')
 
-        if(!product) return res.send('Product Not Found!')
+            const { day, hour, minutes, month } = date(product.updated_at)
 
-        const { day, hour, minutes, month } = date(product.updated_at)
+            product.published = {
+                day: `${day}/${month}`,
+                hour: `${hour}h${minutes}`
+            }
 
-        product.published = {
-            day: `${day}/${month}`,
-            hour: `${hour}h${minutes}`
+            product.oldPrice = formatPrice(product.old_price)
+            product.price = formatPrice(product.price)
+
+            let files = await Procuct.files(product.id)
+            files = files.map(file => ({
+                ...file,
+                src: `${req.protocol}://${req.headers.host}${file.path.replace('public','')}`
+            }))
+
+            return res.render("products/show", { product, files })
+        } catch (error) {
+            console.error(error)
         }
-
-        product.oldPrice = formatPrice(product.old_price)
-        product.price = formatPrice(product.price)
-
-        results = await Procuct.files(product.id)
-        const files = results.rows.map(file => ({
-            ...file,
-            src: `${req.protocol}://${req.headers.host}${file.path.replace('public','')}`
-        }))
-
-        return res.render("products/show", { product, files })
+        
     },
     async edit(req, res) {
-        let results = await Procuct.find(req.params.id)
-        const product = results.rows[0]
+        try {
+            const product = await Procuct.find(req.params.id)
 
-        if(!product) return res.send("Product not found!")
+            if(!product) return res.send("Product not found!")
 
-        product.old_price = formatPrice(product.old_price)
-        product.price = formatPrice(product.price)
+            product.old_price = formatPrice(product.old_price)
+            product.price = formatPrice(product.price)
 
-        //get categories
-        results = await Category.all()
-        const categories = results.rows
+            //get categories
+            const categories = await Category.findAll()
 
-        //get images
-        results = await Product.files(product.id)
-        let files = results.rows
-        files = files.map(file => ({
-            ...file,
-            src: `${req.protocol}://${req.headers.host}${file.path.replace('public','')}`
-        }))
+            //get images
+            let files = await Product.files(product.id)
+            files = files.map(file => ({
+                ...file,
+                src: `${req.protocol}://${req.headers.host}${file.path.replace('public','')}`
+            }))
 
-        return res.render("products/edit.njk", {product, categories, files})
+            return res.render("products/edit", {product, categories, files})
+        } catch (error) {
+            console.error(error)
+        }
     },
     async put(req, res) {
-        const keys = Object.keys(req.body)
+        try {
+            const keys = Object.keys(req.body)
 
-        for(key of keys) {
-            if (req.body[key] == "" && key != "removed_files") {
-                return res.send('Please, fill all fields!')
+            for(key of keys) {
+                if (req.body[key] == "" && key != "removed_files") {
+                    return res.send('Please, fill all fields!')
+                }
             }
+
+            if (req.files.length !=0) {
+                const newFilesPromise = req.files.map(file => 
+                    File.create({...file, product_id: req.body.id}))
+                
+                await Promise.all(newFilesPromise)
+            }
+
+            if (req.body.removed_files) {
+                //1,2,3,
+                const removedFiles = req.body.removed_files.split(",") // [1,2,3,]
+                const lastIndex = removedFiles.length -1
+                removedFiles.splice(lastIndex, 1) // [1,2,3]
+
+                const removedFilesPromise = removedFiles.map(id => File.delete(id))
+
+                await Promise.all(removedFilesPromise)
+            }
+
+            req.body.price = req.body.price.replace(/\D/g, "")
+
+            if (req.body.old_price != req.body.price) {
+                const oldProduct = await Procuct.find(req.body.id)
+                req.body.old_price = oldProduct.rows[0].price
+            }
+
+            await Procuct.update(req.body.id, {
+                category_id: req.body.category_id,
+                name: req.body.name,
+                description: req.body.description,
+                category_id: req.body.old_price,
+                price: req.body.price,
+                quantity: req.body.quantity,
+                status: req.body.status,
+            })
+
+            return res.redirect(`/products/${req.body.id}`)
+        } catch (error) {
+            console.error(error)
         }
-
-        if (req.files.length !=0) {
-            const newFilesPromise = req.files.map(file => 
-                File.create({...file, product_id: req.body.id}))
-            
-            await Promise.all(newFilesPromise)
-        }
-
-        if (req.body.removed_files) {
-            //1,2,3,
-            const removedFiles = req.body.removed_files.split(",") // [1,2,3,]
-            const lastIndex = removedFiles.length -1
-            removedFiles.splice(lastIndex, 1) // [1,2,3]
-
-            const removedFilesPromise = removedFiles.map(id => File.delete(id))
-
-            await Promise.all(removedFilesPromise)
-        }
-
-        req.body.price = req.body.price.replace(/\D/g, "")
-
-        if (req.body.old_price != req.body.price) {
-            const oldProduct = await Procuct.find(req.body.id)
-            req.body.old_price = oldProduct.rows[0].price
-        }
-
-        await Procuct.update(req.body)
-
-        return res.redirect(`/products/${req.body.id}`)
     },
     async delete(req, res) {
         await Procuct.delete(req.body.id)
