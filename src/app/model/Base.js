d3c3cc84e61dbc5555b6e65df0f019601d9a7a03
Luan <luan10017@hotmail.com>
2021-01-24 20:45:14 -0300

    Adicionei espaços na string da query do findOne

--- src/app/model/Base.js
+++ src/app/model/Base.js
@@ -1,19 +1,18 @@
 const db = require('../../config/db')
 
 function find(filters, table) {
-    let query = `SELECT * FROM ${table}`
+    let query = `SELECT * FROM ${table} `
  
     if(filters) {
         Object.keys(filters).map(key => {
             //WHERE | OR | AND
-            query += `${key}`
+            query += ` ${key}`
     
             Object.keys(filters[key]).map(field => {
-                 query += `${field} = '${filters[key][field]}'`
+                 query += ` ${field} = '${filters[key][field]}'`
             })
         })
     }
-
     return db.query(query)
 }
 
